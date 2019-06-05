b1cfebc14f3126cb08201952b7f647571d539615
Jefferson Rylee <rylee.jeff385@gmail.com>
2019-06-05 14:56:08 +0800

    Add scripts and add api key generator

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+$router->get('/key', function() {
+    // generate app key, duh
+    return str_random(32);
+});
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
