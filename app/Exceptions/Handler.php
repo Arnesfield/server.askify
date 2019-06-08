77406ae1137adaa5a39f818990ef26daac21f1ce
Jefferson Rylee <rylee.jeff385@gmail.com>
2019-06-08 08:12:42 +0800

    Handle method not allowed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,7 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -63,6 +64,12 @@ class Handler extends ExceptionHandler
             $res = $errors[0] ?? $msg;
 
             return jresponse($res, $exception->status);
+        }
+
+        if (env('APP_ENV') === 'production') {
+            if ($exception instanceof MethodNotAllowedHttpException) {
+                return jresponse('Unable to deliver your request.', 405);
+            }
         }
 
         return parent::render($request, $exception);
